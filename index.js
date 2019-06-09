7387ecf433f571fdc539f1c016d0f42d2916f7cb
Narongdej Sarnsuwan <s.narongdej@thegang.tech>
2019-06-09 22:13:08 +0700

    Update the comment

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+/* global hexo */
+
 /*
-* global hexo
-* {% asciinima [video_id] %} %}
+* {% asciinima [video_id] %}
 */
 
 hexo.extend.tag.register('asciinema', function(args){
-  videoId = args[0];
+  const videoId = args[0];
   return "<script type=\"text/javascript\" src=\"https://asciinema.org/a/" + videoId + ".js\" id=\"asciicast-"+ videoId + "\" async></script>";
 });
